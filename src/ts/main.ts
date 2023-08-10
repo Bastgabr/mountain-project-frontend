e2e178de99809736171a9df7db7ff04e0d899397
Bastien Gabriel <bastiengabr@gmail.com>
2023-08-10 19:58:24 +0200

    Add map animation on main page

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,8 +1,120 @@
 
-$("#about-button").on("click",function(){
+
+$("#navbar-menu").on("click",'#about-button', function () {
   window.location.href = 'about.html';
 });
 
-$("#explore-map-section").on("click",function(){
-
+// HOME BUTTON Click 
+$("#navbar-menu").on("click","#home-button", function () {
+  //Scroll up
+  $("#content").animate({ scrollTop: 0 }, "smooth");  
 });
+
+// RETURN BUTTON Click
+$("#navbar-menu").on("click","#return-button", function () {
+    //Scroll up
+    $("#content").animate({ scrollTop: 0 }, "smooth");
+    $('#content').css({ "overflow": "auto" });
+    
+      ToggleExploreMapText();
+      $(".iframe").css({ "pointer-events": "none" });
+      RestoreMapHeight();
+      ChangeNavBarMenuItems(`
+      <a id="home-button">Home</a>
+      <a id="maps-button">Maps</a>
+      <a id="82x400-button">82x4000</a>
+      <a id="about-button">About</a>
+    `);
+    
+});
+
+$("#navbar-menu").on("click","#find-4000-button", function () {
+  alert("Héhéhéhéhé");
+});
+
+
+
+$("#explore-map-section").on("click", function () {
+
+      //Scroll to focus element
+      ScrollContentToElement("explore-map-section");
+      ShowMapFullScreen();
+      //Hide scrollbar 
+      $('#content').css({ "overflow": "hidden" });
+
+      NavBarMenuMapMode();
+
+      //Toggle
+      ToggleExploreMapText();
+      $(".iframe").css({ "pointer-events": "auto" });
+});
+
+export function ToggleExploreMapText() {
+  let width = $('#explore-map-text').width();
+  let left = $('#explore-map-text').position().left;
+  let total = width + left;
+  $('#explore-map-text').css({
+      "transform": "translate(-" + total + "px)",
+      "transition": "transform 1000ms ease-in-out"
+  });
+}
+
+export function ScrollContentToElement(elementName) {
+  $("#" + elementName).get(0).scrollIntoView({ behavior: 'smooth' });
+}
+export function ShowMapFullScreen() {
+  let mapFrame = $("iframe");
+  mapFrame.height($("#content").height());
+}
+export function RestoreMapHeight() {
+  let mapFrame = $("iframe");
+  mapFrame.height("");
+}
+
+export function NavBarMenuMapMode() {
+  ChangeNavBarMenuItems(`
+    <a id="return-button">Return</a>
+    <a id="find-4000-button">Find a 4000</a>
+  `);
+}
+
+
+export function ChangeNavBarMenuItems(htmlMenuItems: string){
+  //Hide the navbar menu
+  let navbarMenu = $('#navbar-menu');
+  let width = navbarMenu.width();
+  let left = navbarMenu.position().left;
+  let total = width + left;
+
+  //Hide the old content of the menu
+  navbarMenu.css({
+      "transform": "translate(-" + total + "px)",
+  });
+
+  let count = 0;
+  //When the hiding animation is over, show the new menu
+  navbarMenu.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',   
+  function(e) {
+    console.log("Triggered " + count);
+    if(count == 0){
+      navbarMenu.html(htmlMenuItems);
+      navbarMenu.css({
+        "transform":"translate(0)",
+        });
+    }
+    count++;
+
+  });
+}
+
+
+
+export function ResetNavBarMenu() {
+  $("#nav-menu").html(`
+  <a id="home-button">Home</a>
+  <a id="maps-button">Maps</a>
+  <a id="82x400-button">82x4000</a>
+  <a id="about-button">About</a>
+`);
+}
+//# sourceMappingURL=main.js.map
