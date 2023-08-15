ce77922886df1c07117808eb75a091a5658ecd26
Bastien Gabriel <bastiengabr@gmail.com>
2023-08-15 12:45:58 +0200

    Correct name input verification on about page

--- src/ts/about.js
+++ src/ts/about.js
@@ -9,4 +9,27 @@ $("#submit-button").on('click', function () {
     //Check the entries 
     alert("Ok");
 });
+$("#name-input").on("focusout", function () {
+    let inputField = $("#name-input");
+    if (inputField.val() == "") {
+        inputField.css('border');
+        inputField.css({
+            border: '2px solid #b20a37',
+            color: '#b20a37',
+        });
+    }
+    else {
+        inputField.css({
+            border: '',
+            color: '',
+        });
+    }
+});
+$("#name-input").on("focusin", function () {
+    let inputField = $("#name-input");
+    inputField.css({
+        border: '',
+        color: '',
+    });
+});
 //# sourceMappingURL=about.js.map
--- src/ts/about.ts
+++ src/ts/about.ts
@@ -13,3 +13,28 @@ $("#submit-button").on('click', function(){
   alert("Ok");
 
 });
+
+$("#name-input").on("focusout", function(){
+  let inputField = $("#name-input");
+  if(inputField.val() == ""){
+    inputField.css('border');
+    inputField.css({
+      border: '2px solid #b20a37',
+      color: '#b20a37',
+    });
+  }
+  else{
+    inputField.css({
+      border: '',
+      color: '',
+    });
+  }
+});
+
+$("#name-input").on("focusin", function(){
+  let inputField = $("#name-input");
+  inputField.css({
+    border: '',
+    color: '',
+  });
+})
